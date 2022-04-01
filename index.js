f1ee725413198de1f5487b262dc365b5e66568e3
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-04-01 19:03:33 -0400

    FInished

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+  function superbowlWin(teamRecord) {
+    record.find(year => year === "2018" )
+  }
+
+ 
+
